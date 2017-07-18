e74671cfb4a7a4935a414aba0c06425866ddb0f2
Kyle Richmond <kylerichmond79@gmail.com>
2017-07-18 09:00:16 -0700

    Kyle drove and Gavin navigated to finish the TODO's

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -50,18 +50,19 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(); //TODO: What do we pass in to loadAll()?
-    //TODO: What method do we call to render the index page?
+    Article.loadAll(JSON.parse(localStorage.getItem('rawData'))); //DONE: What do we pass in to loadAll()?
+    //DONE: What method do we call to render the index page?
+    articleView.initIndexPage();
   } else {
-    // TODO: When we don't already have the rawData,
+    // DONE: When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
-    $.get('/data/hackeripsum.json')
+    $.getJSON('data/hackerIpsum.json')
     .then(
       function(data) {
-        console.log(data);
+        localStorage.setItem('rawData', JSON.stringify(data));
       },
 
       function(err) {
@@ -69,18 +70,6 @@ Article.fetchAll = function() {
       }
     )
 
-  //   let successCallBack = function(data) {
-  //   let template = Handlebars.compile($('#template').html());
-  //   $('#app').html(template({person: data.results});
-  //
-  // };
-  //
-  //
-  // let errorCallback = function(err) {
-  //
-  // };
-  // $.getJSON('https://swapi.co/api/people')
-  //   .then(successCallBack, errorCallback);
 
 
 
